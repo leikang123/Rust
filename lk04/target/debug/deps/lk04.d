194ae431078e1a0e0490cea7f3d886e8867090ab
leikang123 <1013626230@qq.com>
2021-09-08 15:27:58 +0800

    rust demo explme

---
+++ lk04/target/debug/deps/lk04.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/lk04/target/debug/deps/lk04: src/main.rs
+
+/Users/mac1094/rust/lk04/target/debug/deps/lk04.d: src/main.rs
+
+src/main.rs:
---
+++ lk04/target/debug/lk04.d
@@ -0,0 +1,1 @@
+/Users/mac1094/rust/lk04/target/debug/lk04: /Users/mac1094/rust/lk04/src/main.rs
