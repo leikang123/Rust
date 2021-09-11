744892293f56175479f8376c1e5aa5ded9a3a3c2
leikang123 <1013626230@qq.com>
2021-09-11 18:42:25 +0800

    rust demo

---
+++ lk12/target/debug/deps/lk12.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/lk12/target/debug/deps/lk12: src/main.rs
+
+/Users/mac1094/rust/lk12/target/debug/deps/lk12.d: src/main.rs
+
+src/main.rs:
---
+++ lk12/target/debug/lk12.d
@@ -0,0 +1,1 @@
+/Users/mac1094/rust/lk12/target/debug/lk12: /Users/mac1094/rust/lk12/src/main.rs
