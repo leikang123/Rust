13b02c05763e4bb1441a1bcf675153aa6f5cb23d
leikang123 <1013626230@qq.com>
2021-09-12 17:29:27 +0800

    rust demo

---
+++ lk17/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+let x = "leikang";
+let _found_x = match &x {
+  "leikang" =>"jingfeng",
+  "kjh" => "ehdjg",
+  "opio" => "vbgdu"
+};
+  
+    println!("x value {}",x);
+}
---
+++ lk17/target/debug/deps/lk17.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/lk17/target/debug/deps/lk17: src/main.rs
+
+/Users/mac1094/rust/lk17/target/debug/deps/lk17.d: src/main.rs
+
+src/main.rs:
