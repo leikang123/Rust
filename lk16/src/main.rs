092fd185c4550dca362c930cb5a5f58d465bf9a8
leikang123 <1013626230@qq.com>
2021-09-12 17:28:38 +0800

    rust

---
+++ lk16/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+let x = 12;
+match x {
+ 1 =>println!("leikang"),
+ 2 =>println!("个环境换个"),
+ 3 =>println!("是否会"),
+ 12 => println!("ok"),
+ _ => println!("Hello, world!"),
+};
+}
---
+++ lk16/target/debug/lk16.d
@@ -0,0 +1,1 @@
+/Users/mac1094/rust/lk16/target/debug/lk16: /Users/mac1094/rust/lk16/src/main.rs
