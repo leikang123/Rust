2a70479b1ae89f0a6523633fcd95a1e63006374c
leikang123 <1013626230@qq.com>
2021-09-15 18:57:41 +0800

    rust

---
+++ lk35/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+let x = vec![1,2,3,4,5,9];
+let _x_skice :&[i64] = &x[2 ..4];
+
+    println!("x is value {:?}",_x_skice);
+
+}
---
+++ lk35/target/debug/lk35.d
@@ -0,0 +1,1 @@
+/Users/mac1094/rust/lk35/target/debug/lk35: /Users/mac1094/rust/lk35/src/main.rs
