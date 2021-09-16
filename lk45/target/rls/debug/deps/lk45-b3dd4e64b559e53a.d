4273b18a4dc7744c07c702618552562a422dd879
leikang123 <1013626230@qq.com>
2021-09-16 21:00:42 +0800

    rust demo

---
+++ lk45/target/rls/debug/deps/lk45-b3dd4e64b559e53a.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/lk45/target/rls/debug/deps/lk45-b3dd4e64b559e53a.rmeta: src/main.rs
+
+/Users/mac1094/rust/lk45/target/rls/debug/deps/lk45-b3dd4e64b559e53a.d: src/main.rs
+
+src/main.rs:
