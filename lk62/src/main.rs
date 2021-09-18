4fe682c8e8f7e984b4cd151313690dc248361f75
leikang123 <1013626230@qq.com>
2021-09-18 17:20:54 +0800

    rust demo

---
+++ lk62/src/main.rs
@@ -0,0 +1,5 @@
+first_unit_test.rs 
+#[test]
+fn base_test(){
+    assert!(true);
+}
---
+++ lk62/target/rls/debug/deps/lk62-8a84886d6f440aa4.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/lk62/target/rls/debug/deps/lk62-8a84886d6f440aa4.rmeta: src/main.rs
+
+/Users/mac1094/rust/lk62/target/rls/debug/deps/lk62-8a84886d6f440aa4.d: src/main.rs
+
+src/main.rs:
