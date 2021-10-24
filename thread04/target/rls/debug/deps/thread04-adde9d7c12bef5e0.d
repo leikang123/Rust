d7fcb5f10a34d3a5dfa5c38477a7bbd4d841c38c
leikang123 <1013626230@qq.com>
2021-10-24 10:42:30 +0800

    demo

---
+++ thread04/target/rls/debug/deps/thread04-adde9d7c12bef5e0.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread04/target/rls/debug/deps/thread04-adde9d7c12bef5e0.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread04/target/rls/debug/deps/thread04-adde9d7c12bef5e0.d: src/main.rs
+
+src/main.rs:
