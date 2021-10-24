eac5943e6a3ebd01029599d402f6928af9dbb8be
leikang123 <1013626230@qq.com>
2021-10-24 10:43:04 +0800

    demo

---
+++ thread05/target/rls/debug/deps/thread05-430772ad0414c5e5.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread05/target/rls/debug/deps/thread05-430772ad0414c5e5.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread05/target/rls/debug/deps/thread05-430772ad0414c5e5.d: src/main.rs
+
+src/main.rs:
