8c7767ee6f59e86d24d25e1e7b61d144bb0ed5cb
leikang123 <1013626230@qq.com>
2021-11-04 20:57:37 +0800

    demo

---
+++ thread08/target/rls/debug/deps/thread08-2c27c8728691518d.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread08/target/rls/debug/deps/thread08-2c27c8728691518d.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread08/target/rls/debug/deps/thread08-2c27c8728691518d.d: src/main.rs
+
+src/main.rs:
