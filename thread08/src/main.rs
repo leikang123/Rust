1bbcca52be575aba3552dd813f98a8a20a8b4d20
leikang123 <1013626230@qq.com>
2021-11-05 16:24:02 +0800

    update

--- thread08/src/main.rs
+++ thread08/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::Read;
+use std:: sync::RwLock;
+use std:: thread;
+
+/**
+ * read只是提供对线程的读取访问权限，可以多个读取调用
+ * write只是提供对线程的独占访问，将数据写入包装类型
+ * 从RWLOCK实力到线程只能有一个线程写入访问权限
+ */
 fn main() {
-    println!("Hello, world!");
+    let m = RwLock::new(5);
+    let c = thread::spawn(move || {
+        {
+            *m.write().unwrap() +=1;
+        }
+        let updated =*m.read().unwrap();
+        updated
+
+    });
+    let updated = c.join().unwrap();
+    println!("{:?}",updated);
 }
---
+++ thread08/target/debug/deps/thread08-16ef5b3a0d151a9d.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread08/target/debug/deps/thread08-16ef5b3a0d151a9d.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread08/target/debug/deps/thread08-16ef5b3a0d151a9d.d: src/main.rs
+
+src/main.rs:
