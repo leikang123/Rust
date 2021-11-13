d800fb965f3caaa79038e553e65ab8589a05e097
leikang123 <1013626230@qq.com>
2021-11-13 20:46:44 +0800

    demo

--- thread09/src/main.rs
+++ thread09/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+/**
+ * 通过消息传递进行通信
+ * 异步通信
+ * 
+ */
+// 简单的生产者和消费者演示案例，主要是在主线程生成值0-9，然后在新生成线程输出
+use std::{sync::mpsc::channel, thread};
+
+
+ fn main() {
+     let (tx,rx)= channel();
+     let join_handle =thread::spawn(move || {
+         while let Ok(n) = rx.recv(){
+             println!("Recoved{}",n)
+         }
+         
+     });
+     for i in 0 ..10{
+         tx.send(i).unwrap();
+     }
+     join_handle.join().unwrap();
+    // println!("Hello, world!");
+    
 }
