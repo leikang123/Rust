59dfe2bc2434c96513e8927043828aff0d18f24a
leikang123 <1013626230@qq.com>
2021-12-02 17:49:02 +0800

    update

---
+++ thread07/target/debug/deps/thread07-4725a4e893ce6ce3.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread07/target/debug/deps/thread07-4725a4e893ce6ce3.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread07/target/debug/deps/thread07-4725a4e893ce6ce3.d: src/main.rs
+
+src/main.rs:
