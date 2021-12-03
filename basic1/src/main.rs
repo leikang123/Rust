495ad30563a6243483325187af10acf3499eb0fa
leikang123 <1013626230@qq.com>
2021-12-03 16:27:06 +0800

    update

--- basic1/src/main.rs
+++ basic1/src/main.rs
@@ -1,7 +1,21 @@
-
+// 结构体代码实现
+struct Student{
+    name: &'static str,
+    socre :i32
+}
+struct Color(i32,i32,i32);
 fn main() {
- let big_val = std::i32::MAX;
-let x = big_val +1;
-    println!("{:?}",x);
+    let b_b = Color(23,45,78);
+let socre =43;
+let name = "lk";
 
+// 结构体实力话
+let mut s  = Student{
+    name:"iou",
+    socre:98
+};
+// s.socre = 98;
+let s1 = s.name;
+let s2 = s.socre;
+ println!("name:{},socre:{}",s1,s2)
 }
