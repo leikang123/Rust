a3f18fdee4f3429f8fc5e29e96b72d9f38162953
leikang123 <1013626230@qq.com>
2021-12-06 17:28:45 +0800

    demo

---
+++ enum2/target/debug/deps/enum2-34559a3314a65bda.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/enum2/target/debug/deps/enum2-34559a3314a65bda.rmeta: src/main.rs
+
+/Users/mac1094/rust/enum2/target/debug/deps/enum2-34559a3314a65bda.d: src/main.rs
+
+src/main.rs:
---
+++ enum2/target/debug/deps/enum2-4ededfe9a47eeda2.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/enum2/target/debug/deps/enum2-4ededfe9a47eeda2.rmeta: src/main.rs
+
+/Users/mac1094/rust/enum2/target/debug/deps/enum2-4ededfe9a47eeda2.d: src/main.rs
+
+src/main.rs:
