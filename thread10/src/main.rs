90460ee7a34b99a2a66f8e3045b32d2b8a428d31
leikang123 <1013626230@qq.com>
2021-12-08 19:44:01 +0800

    demo

---
+++ thread10/src/main.rs
@@ -0,0 +1,17 @@
+use std::thread;
+use std::time;
+
+fn main() {
+    // 线程开始时间
+
+    let start  = time::Instant::now();
+    // 创建一个线程
+    let handle = std::thread::spawn(move || {
+        // 变量panic时间100秒
+        let panic =time::Duration::from_millis(100);
+        thread::sleep(panic.clone());  
+    });
+    handle.join().unwrap();
+    let finsh = time::Instant::now();
+    println!("{:02?}",finsh.duration_since(start));
+}
---
+++ thread10/target/rls/debug/deps/thread10-9d48841fe94ae59f.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread10/target/rls/debug/deps/thread10-9d48841fe94ae59f.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread10/target/rls/debug/deps/thread10-9d48841fe94ae59f.d: src/main.rs
+
+src/main.rs:
