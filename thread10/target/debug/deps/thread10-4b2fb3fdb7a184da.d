aad2ca86cb8cdf7bbababce88d1b8e63fabb08e1
leikang123 <1013626230@qq.com>
2021-12-11 18:13:06 +0800

    update

---
+++ thread10/target/debug/deps/thread10-4b2fb3fdb7a184da.d
@@ -0,0 +1,5 @@
+/Users/mac1094/rust/thread10/target/debug/deps/thread10-4b2fb3fdb7a184da.rmeta: src/main.rs
+
+/Users/mac1094/rust/thread10/target/debug/deps/thread10-4b2fb3fdb7a184da.d: src/main.rs
+
+src/main.rs:
